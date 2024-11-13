8388af1130ee2fd7bd89ff7fa2d1fae3b3cd59ae
duyquangbui99 <duyquangbui99@gmail.com>
2024-11-12 21:27:28 -0600

    solution

--- app.js
+++ app.js
@@ -1,29 +1,58 @@
 (function () {
     'use strict';
 
-    var app = angular.module('lunchApp', []);
+    angular.module('ShoppingListCheckOff', [])
+        .controller('ToBuyController', ToBuyController)
+        .controller('AlreadyBoughtController', AlreadyBoughtController)
+        .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-    var LunchCheckController = function ($scope) {
-        $scope.message = "Welcome to the Lunch Checker!";
-        $scope.messageStyle = {};
+    // #1 - ToBuyController
+    ToBuyController.$inject = ['ShoppingListCheckOffService'];
+    function ToBuyController(ShoppingListCheckOffService) {
+        var toBuy = this;
 
-        $scope.checkLunch = function () {
-            if ($scope.lunchMenu) {
-                var items = $scope.lunchMenu.split(',');
-                if (items.length > 3) {
-                    $scope.message = 'Too much!';
-                    $scope.messageStyle = { color: 'green' }
-                } else {
-                    $scope.message = 'Enjoy!';
-                    $scope.messageStyle = { color: 'green' }
-                }
-            } else {
-                $scope.message = 'Please enter data first';
-                $scope.messageStyle = { color: 'red' }
-            }
-        }
-    };
+        toBuy.items = ShoppingListCheckOffService.getBuyItems();
 
-    LunchCheckController.$inject = ['$scope'];
-    app.controller('LunchCheckController', LunchCheckController)
+        toBuy.buyItem = function (itemIndex) {
+            ShoppingListCheckOffService.buyItem(itemIndex);
+        };
+    }
+
+    // #2 - AlreadyBoughtController
+    AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+    function AlreadyBoughtController(ShoppingListCheckOffService) {
+        var alreadyBought = this;
+
+        alreadyBought.items = ShoppingListCheckOffService.getBoughtItems();
+    }
+
+    // Define the service for data sharing
+    function ShoppingListCheckOffService() {
+        var service = this;
+
+        var toBuyItems = [
+            { name: "cookies", quantity: 10 },
+            { name: "milk", quantity: 2 },
+            { name: "bread", quantity: 1 },
+            { name: "cheese", quantity: 5 },
+            { name: "chocolate", quantity: 3 }
+        ];
+
+        var boughtItems = [];
+
+        service.getBuyItems = function () {
+            return toBuyItems;
+        };
+
+        service.getBoughtItems = function () {
+            return boughtItems;
+        };
+
+        // Move item from "To Buy" list to "Already Bought" list
+        service.buyItem = function (itemIndex) {
+            var item = toBuyItems.splice(itemIndex, 1)[0];
+            boughtItems.push(item);
+        };
+    }
+
 })();
